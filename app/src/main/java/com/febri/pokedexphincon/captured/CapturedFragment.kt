cf0e21e7f2f154909d103d74a19e8a9ad2a0bec0
Febriansyah <febrian411@gmail.com>
2023-10-12 10:38:11 +0700

    modify captured page

--- app/src/main/java/com/febri/pokedexphincon/captured/CapturedFragment.kt
+++ app/src/main/java/com/febri/pokedexphincon/captured/CapturedFragment.kt
@@ -5,11 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.febri.pokedexphincon.databinding.FragmentHomeBinding
+import com.febri.pokedexphincon.databinding.FragmentCapturedBinding
 
 class CapturedFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentCapturedBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -20,7 +20,7 @@ class CapturedFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentCapturedBinding.inflate(inflater, container, false)
         return binding.root
     }
 
